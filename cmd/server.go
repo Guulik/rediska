19ce6d512ac6fa90ba447262a51751721328e001
Sasha <gulixandr@gmail.com>
2024-11-05 22:19:17 +0300

    feat: copied slog from another project

--- cmd/server.go
+++ cmd/server.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
 	"net"
+	"os"
 	"rediska/config"
 	"rediska/internal/Storage"
 	"rediska/internal/api"
+	"rediska/internal/lib/logger/handlers/slogpretty"
 )
 
 func main() {
 	cfg := config.MustLoad()
+
+	_ = setupLogger("local")
 
 	address := fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)
 	l, err := net.Listen("tcp", address)
@@ -28,4 +33,31 @@ func main() {
 		}
 		go api.ReadInput(con)
 	}
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "local":
+		log = setupPrettySlog()
+	case "prod":
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
