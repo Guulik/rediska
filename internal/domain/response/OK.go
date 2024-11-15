3034310e4fd78b530dbcde2c84b41edfd84c5e5d
Sasha <gulixandr@gmail.com>
2024-11-15 12:12:36 +0300

    FIX: cyclic deps просто вынес ответы в пакет domain/response

--- internal/domain/response/OK.go
+++ internal/domain/response/OK.go
@@ -1,23 +1,10 @@
-package api
+package response
 
 import (
 	"fmt"
-	"rediska/internal/util/resper"
-
 	"net"
+	"rediska/internal/util/resper"
 )
-
-func PONG(conn net.Conn) {
-	buf, err := resper.EncodeSimpleString("PONG")
-	if err != nil {
-		fmt.Println("failed to encode:", err)
-	}
-
-	_, err = conn.Write(buf.Bytes())
-	if err != nil {
-		fmt.Println("failed to write PONG to client")
-	}
-}
 
 func OK(conn net.Conn) {
 	op := "OK response"
