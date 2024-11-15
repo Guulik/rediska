a59efc0d46678d49d0af87829a68534d267d28f4
Sasha <gulixandr@gmail.com>
2024-11-15 15:04:37 +0300

    TEST: test commit for pull req. Added compression for required command and parsed command

--- internal/api/InputHandler_test.go
+++ internal/api/InputHandler_test.go
@@ -1,47 +1,40 @@
 package api
 
 import (
-	"github.com/tidwall/resp"
+	"github.com/stretchr/testify/require"
 	"log/slog"
 	"net"
-	"reflect"
 	"testing"
 )
 
 func TestAPI_readInput(t *testing.T) {
-	type fields struct {
+	type ApiFields struct {
 		log  *slog.Logger
 		conn net.Conn
 	}
-	type args struct {
-		conn    net.Conn
-		message []byte
-	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    resp.Value
-		wantErr bool
+		name        string
+		apiFields   ApiFields
+		message     []byte
+		wantCommand string
 	}{
 		{
 			name: "ping",
+
+			wantCommand: "ping",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			a := &API{
-				log:  tt.fields.log,
-				conn: tt.fields.conn,
+				log:  tt.apiFields.log,
+				conn: tt.apiFields.conn,
 			}
-			got, err := a.readInput(tt.args.conn)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("readInput() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("readInput() got = %v, want %v", got, tt.want)
-			}
+			got, err := a.readInput(tt.apiFields.conn)
+
+			require.NoError(t, err)
+			command := got.Array()[0].String()
+			require.Equal(t, tt.wantCommand, command)
 		})
 	}
 }
