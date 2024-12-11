68c35e5ed45ff65a26fa4cd5430b8342091d85da
Sasha <gulixandr@gmail.com>
2024-12-11 23:11:01 +0300

    FIX: now ECHO returns Bulk string

--- internal/service/ECHO.go
+++ internal/service/ECHO.go
@@ -8,7 +8,7 @@ import (
 func (s *CommandsService) ECHO(phrase string) (bytes.Buffer, error) {
 	log := s.log.With("op", "Service.ECHO")
 
-	buf, err := resper.EncodeSimpleString(phrase)
+	buf, err := resper.EncodeBulkString(phrase)
 	if err != nil {
 		log.Error("failed to encode:", err)
 		return bytes.Buffer{}, err
